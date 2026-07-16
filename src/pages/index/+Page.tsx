75994f38103b7e4cc2290c161a7682b4dda73c4b
rtritto <ariele.1994@gmail.com>
2026-07-16 11:09:45 +0200

    feat: improve checkbox example

--- src/pages/index/+Page.tsx
+++ src/pages/index/+Page.tsx
@@ -1,9 +1,22 @@
+import { createSignal } from 'solid-js'
+
 function Page() {
+  const [isChecked, setIsChecked] = createSignal(true)
+
   return (
     <>
-      <h1>My Vike + Solid App</h1>
-      This page is:
-      <input type="checkbox" checked class="checkbox" />
+      <h1>App: Vike Lite + Solid</h1>
+
+      <input
+        type="checkbox"
+        class="checkbox"
+        checked={isChecked()}
+        onChange={(event) => setIsChecked(event.currentTarget.checked)}
+      />
+
+      <br />
+
+      The checkbox is: {isChecked() ? 'checked' : 'unchecked'}
     </>
   )
 }
